7a9534d94f7207c8c9d4d8e6430c0ded912d81ae
LISA_COOK <lisabpink@gmail.com>
2020-06-12 11:42:13 -0400

    ring_buffer.py- append and get tests passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -13,8 +13,11 @@ class RingBuffer:
 
 # Appends/Adds the given element to the buffer
     def append(self, item):
-        pass
+        self.storage[self.current] = item
+        self.current += 1
+        if self.current == self.capacity:
+            self.current = 0
 
  # Get/Return all of the elements in the buffer in a list in their given order
     def get(self):
-        pass
+        return [x for x in self.storage if x is not None]
