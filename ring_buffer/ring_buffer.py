79e774913be14522760d1b2b2fe0bc5ac3ec7f34
LISA_COOK <lisabpink@gmail.com>
2020-06-12 13:51:53 -0400

    Started converting Array to Stack in ring_buffer.py- ran out of time

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,10 +4,14 @@
 
 # Just read the rubric and relized it is asking for either Stack or Queue and NOT Array.
 # Will need to loop back after reverse.py to implement Stack LIFO- Last In First Out
+from doubly_linked_list import DoublyLinkedList
 
 # Ring buffer is a non-growable buffer with a fixed size.
 # When the ring buffer is full and a new element is inserted,
 # the oldest element in the ring buffer is overwritten with the newest element.
+# ARRAY - ALL TESTS PASSING
+
+
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
@@ -24,3 +28,44 @@ class RingBuffer:
  # Get/Return all of the elements in the buffer in a list in their given order
     def get(self):
         return [x for x in self.storage if x is not None]
+
+# STACK- 2 TEST PASSING- RAN OUT OF TIME
+
+
+class RingBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.storage = DoublyLinkedList()
+        self.current = None
+
+# Appends/Adds the given element to the buffer
+    def append(self, item):
+        # is list at capacity?
+        if self.capacity > self.storage.length:
+            # no = add to tail
+            self.storage.add_to_tail(item)
+            # if there - replace with current value
+            if self.storage.length == 1:
+                self.current = self.storage.head
+            else:
+                # set current value to node
+                self.current.value = item
+            # if  not tail- move to next
+            if self.current is not self.storage.tail:
+                self.current = self.current.next
+            else:
+                self.current = self.storage.head
+
+ # Get/Return all of the elements in the buffer in a list in their given order
+    def get(self):
+        # create empty list -loop through nodes
+        content_list = []
+        # while loop traversing through the list
+        node = self.storage.head
+        # adding value to the list once loop is done
+        while node is not None:
+            content_list.append(node.value)
+            # move to next value
+            node = node.next
+        # return  list
+        return content_list
