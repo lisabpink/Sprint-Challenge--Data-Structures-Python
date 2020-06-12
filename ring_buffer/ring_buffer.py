3d3137b6c4ec180b78de22b445253606f96c9f05
LISA_COOK <lisabpink@gmail.com>
2020-06-12 11:29:43 -0400

    ring_buffer.py- added planning notes, capacity test passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
+# PLANNING
+# Use Array because it is the most space efficient, great run time complexity,
+# only edits the last item and the length of Array is not changing.
+
+# Ring buffer is a non-growable buffer with a fixed size.
+# When the ring buffer is full and a new element is inserted,
+# the oldest element in the ring buffer is overwritten with the newest element.
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = [None]*capacity
+        self.current = 0
 
+# Appends/Adds the given element to the buffer
     def append(self, item):
         pass
 
+ # Get/Return all of the elements in the buffer in a list in their given order
     def get(self):
         pass
